f2760b5d2a84fa354fd9a1c6853f16f8997f4e3d
Jake Soenneker <jake@soenneker.com>
2025-09-20 20:03:22 -0500

    Deps

--- test/Soenneker.Quark.Labels.Demo/Program.cs
+++ test/Soenneker.Quark.Labels.Demo/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.Web;
@@ -9,6 +9,7 @@ using Microsoft.JSInterop;
 using Serilog;
 using Serilog.Debugging;
 using Soenneker.Serilog.Sinks.Browser.Blazor.Registrars;
+using Soenneker.Quark.Themes.Registrars;
 
 namespace Soenneker.Quark.Labels.Demo;
 
@@ -30,6 +31,7 @@ public sealed class Program
                 BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
             });
 
+            builder.Services.AddEmptyThemeProviderAsScoped();
 
             WebAssemblyHost host = builder.Build();
 
